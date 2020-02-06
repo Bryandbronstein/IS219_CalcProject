39a6f45ee81c7cb74287b7d984bcbc52703dfc8b
Bryandbronstein <bryandbronstein@gmail.con>
2020-02-06 10:18:55 -0500

    Added square.js and squareroot.js functions Added square.test.js and squareroot.test.js Updated index.js to employ all functions and print results to console

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
 const sum = require('./sum');
-a = 1;
-b = 2;
+const subtract = require('./subtract');
+const multiply = require('./multiply');
+const divide = require('./divide');
+const square = require('./square');
+const squareroot = require('./squareroot');
+
+a = 3;
+b = 6;
+d = 36;
 
 c = sum(a,b);
+console.log(a + " plus " + b + " equals " + c);
 
-console.log("The answer is: " + c);
+c = subtract(b,a);
+console.log(b + " minus " + a + " equals " + c);
+
+c = multiply(a,b);
+console.log(a + " multiplied by " + b + " equals " + c);
+
+c=divide(b,a);
+console.log(b + " divided by " + a + " equals " + c);
+
+c=square(a);
+console.log(a + " squared equals " + c);
+
+c=squareroot(d);
+console.log("Square root of " + d + " equals " + c);
+
+
---
+++ square.test.js
@@ -0,0 +1,5 @@
+const square = require('./square');
+
+test('square 6 to equal 36', () => {
+    expect(square(6)).toBe(36);
+});
